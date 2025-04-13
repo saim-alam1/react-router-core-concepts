f3fdd8adc13e3885dfe0d982b9186361b2cbc7ab
saim-alam1 <allbehassansaim@gmail.com>
2025-04-13 16:16:44 +0600

    Loaded Dynamic Nav Options

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
+import Link from './Link';
+
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about"
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services"
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog"
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact"
+  },
+];
+
 
 const NavBar = () => {
   return (
     <nav>
+
+      <ul className='flex'>
+        {
+          navigationData.map(route => <Link key={route.id} route={route}></Link>)
+        }
+      </ul>
+
+      {/* <ul className='flex'>
+        {
+          navigationData.map(route => <li key={route.id} className='mr-10'>
+            <a href={route.path}>{route.name}</a>
+          </li>)
+        }
+      </ul> */}
 
 
       {/* <ul className='flex'>

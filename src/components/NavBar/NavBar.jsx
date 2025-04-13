a817343577a700404618cd81c6fb4ecacd22fe1d
saim-alam1 <allbehassansaim@gmail.com>
2025-04-13 15:27:34 +0600

    Experimenting NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+
+
+      {/* <ul className='flex'>
+        <li className='mr-10'><a href="/">Home</a></li>
+        <li className='mr-10'><a href="/about">About</a></li>
+        <li className='mr-10'><a href="/blog">Blog</a></li>
+      </ul> */}
+    </nav>
+  );
+};
+
+export default NavBar;

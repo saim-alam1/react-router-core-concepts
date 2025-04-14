d9a51feb700107ee0aff535029f56b5b399b7d2b
saim-alam1 <allbehassansaim@gmail.com>
2025-04-14 12:25:05 +0600

    Replaced Card Component With Modern Cards

--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCards from '../pricingCards/PricingCards';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
 
 const PricingOptions = ({ pricingPromise }) => {
 
@@ -12,9 +13,14 @@ const PricingOptions = ({ pricingPromise }) => {
 
       <h2 className='text-5xl text-center'>Get Our Membership</h2>
 
-      <div className='grid grid-cols-3 gap-8'>
-        {
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+
+        {/* {
           pricingData.map(pricing => <PricingCards key={pricing.id} pricing={pricing}></PricingCards>)
+        } */}
+
+        {
+          pricingData.map(pricing => <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>)
         }
       </div>
 
--- src/components/pricingCards/PricingCards.jsx
+++ src/components/pricingCards/PricingCards.jsx
@@ -4,18 +4,19 @@ import PricingFeatures from './PricingFeatures';
 const PricingCards = ({ pricing }) => {
   const { name, price, description, features } = (pricing);
   return (
-    <div className='border bg-amber-600 rounded-3xl p-4'>
+    <div className='flex flex-col border bg-amber-600 rounded-3xl p-4'>
       {/* Card Header */}
       <h1 className="text-4xl">{name}</h1>
       <h4 className='text-2xl'>{price}</h4>
 
       {/* Card Body */}
-      <div className='bg-amber-800 p-4 rounded-2xl mt-10'>
+      <div className='bg-amber-800 p-4 rounded-2xl mt-10 flex-1'>
         <p>{description}</p>
         {
           features.map((features, index) => <PricingFeatures key={index} features={features}></PricingFeatures>)
         }
       </div>
+      <button className="btn w-full mt-4">Subscribe</button>
     </div>
   );
 };
